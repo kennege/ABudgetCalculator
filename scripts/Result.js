b5b6d0401b65ae7363d671014126bc2fb18295a6
kennege <gerardkennedy93@gmail.com>
2021-07-11 14:25:56 +1000

    Bootstrapping

--- scripts/Result.js
+++ scripts/Result.js
@@ -5,25 +5,45 @@ class Result {
 
   populate_table(bw_pairs) {
     this.bw_pairs = bw_pairs;
-    // generate bucket/income table
-    let table = document.getElementById("tbody");
-    table.innerHTML = "";
+    // first half of bucket/income table
+    let table1 = document.getElementById("tbody_1");
+    table1.innerHTML = "";
     for (let i=0;i<Object.keys(bw_pairs).length;i++){
       if (!bw_pairs[i].bucket.includes("income")) {
-        let row = table.insertRow();
+        let row = table1.insertRow();
         insertTableEntry(row, 0, bw_pairs[i].bucket) // bucket name
-        for (let j=0;j<10;j++){
+        for (let j=0;j<5;j++){
           insertTableEntry(row, j+1, "$"+(anIncome.convert(bw_pairs[i].weight)[j])) 
         }
       }
     }
-    let table_total = document.getElementById('total');
-    table_total.innerHTML = "";
-    let row = table_total.insertRow();
-    insertTableEntry(row, 0, 'Total');
-    for (let j=0;j<10;j++){
-      insertTableEntry(row, j+1, "$"+(anIncome.convert(1)[j])) 
+    let table_total1 = document.getElementById('total1');
+    table_total1.innerHTML = "";
+    let row1 = table_total1.insertRow();
+    insertTableEntry(row1, 0, 'Total');
+    for (let j=0;j<5;j++){
+      insertTableEntry(row1, j+1, "$"+(anIncome.convert(1)[j])) 
     }
+   // second half of bucket/income table
+   let table2 = document.getElementById("tbody_2");
+   table2.innerHTML = "";
+   for (let i=0;i<Object.keys(bw_pairs).length;i++){
+     if (!bw_pairs[i].bucket.includes("income")) {
+       let row = table2.insertRow();
+       insertTableEntry(row, 0, bw_pairs[i].bucket) // bucket name
+       for (let j=0;j<5;j++){
+         insertTableEntry(row, j+1, "$"+(anIncome.convert(bw_pairs[i].weight)[j+5])) 
+       }
+     }
+   }
+   let table_total2 = document.getElementById('total2');
+   table_total2.innerHTML = "";
+   let row2 = table_total2.insertRow();
+   insertTableEntry(row2, 0, 'Total');
+   for (let j=0;j<5;j++){
+     insertTableEntry(row2, j+1, "$"+(anIncome.convert(1)[j+5])) 
+   }
+
     $('#table_box').fadeIn(1000);
   }
 
