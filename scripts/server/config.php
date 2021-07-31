d24d8eb2f4a229371fc24f7b315b445d29dfd399
kennege <gerardkennedy93@gmail.com>
2021-07-31 21:07:30 +1000

    remotemysql

--- scripts/server/config.php
+++ scripts/server/config.php
@@ -4,8 +4,9 @@ define('DB_SERVER', 'remotemysql.com');
 define('DB_USERNAME', '3kcTdYeFXT');
 define('DB_PASSWORD', '3xzrfxC1Ty');
 define('DB_NAME', '3kcTdYeFXT');
+define('DB_PORT', '3306');
  
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
