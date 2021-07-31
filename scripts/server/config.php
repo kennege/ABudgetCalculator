d44a64a0d15fd0f9f864d43bae17a5a2d886f14d
kennege <gerardkennedy93@gmail.com>
2021-07-31 20:16:35 +1000

    remotemysql

--- scripts/server/config.php
+++ scripts/server/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'income');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', '3kcTdYeFXT');
+define('DB_PASSWORD', 'Iz24Odma0T');
+define('DB_NAME', '3kcTdYeFXT');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
